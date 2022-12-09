413bfb2893fbcd4c678d98ed83cd9f0bd4cd5545
Lowell130 <ghost.pizza@gmail.com>
2022-12-09 12:43:14 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,43 @@
 <template>
   <div class="container box my-4">
-
-    <!-- singola linea -->
     <h1 class="text-center m-3">Password Generator</h1>
-    <div class="input-group input-group-lg">   
-     
-    
+    <div class="input-group input-group-lg">
       <button class="btn btn-outline-secondary" @click="generate()" type="button">Generate</button>
       <button class="btn btn-outline-secondary" @click.prevent="copyToClipboard" type="button">Copy</button>
-      
-      <input type="text" class="form-control" v-on:focus="$event.target.select()" ref="generator" v-model="password" id="generator">
+      <input type="text" class="form-control" v-on:focus="$event.target.select()" ref="generator" v-model="password"
+        id="generator">
     </div>
-
     <div class="container my-4 text-center">
-<!-- password Character -->
+      <!-- password Character -->
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="char_types_alpha_numeric" v-model="config.alphaNumeric" id="alpha_numeric">
+        <input class="form-check-input" type="checkbox" name="char_types_alpha_numeric" v-model="config.alphaNumeric"
+          id="alpha_numeric">
         <label class="form-check-label" for="alpha_numeric">Alpha Numeric</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" name="char_types_special_chars" v-model="config.specialChars" id="special_chars">
+        <input class="form-check-input" type="checkbox" name="char_types_special_chars" v-model="config.specialChars"
+          id="special_chars">
         <label class="form-check-label" for="special_chars">Special Chars</label>
       </div>
-
-
-
-<!-- password case -->
+      <!-- password case -->
       <div class="form-check form-check-inline">
-        <input class="form-check-input" name="case" type="radio" id="uppercase" v-model="config.passwordCase" value="uppercase">
+        <input class="form-check-input" name="case" type="radio" id="uppercase" v-model="config.passwordCase"
+          value="uppercase">
         <label class="form-check-label" for="uppercase">Upper Case</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" name="case" type="radio" id="lowercase" v-model="config.passwordCase" value="lowercase">
+        <input class="form-check-input" name="case" type="radio" id="lowercase" v-model="config.passwordCase"
+          value="lowercase">
         <label class="form-check-label" for="lowercase">Lower Case</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" name="case" type="radio" id="mixedcase" v-model="config.passwordCase" value="mixedcase">
+        <input class="form-check-input" name="case" type="radio" id="mixedcase" v-model="config.passwordCase"
+          value="mixedcase">
         <label class="form-check-label" for="mixedcase">Mixed Case</label>
       </div>
-
-
-      <!-- password Lenght -->
-    
-
-
     </div>
-  </div> 
+  </div>
 </template>
-
 <script>
 import Swal from 'sweetalert2';
 export default {
@@ -57,12 +47,10 @@ export default {
       config: {
         passwordCase: 'mixedcase',  // uppercase, lowercase, mixedcase,
         alphaNumeric: true,
-        specialChars: true,    
-        length: 12    
+        specialChars: true,
+        length: 12
       },
-    
     }
-
   },
   methods: {
     generate() {
@@ -98,7 +86,6 @@ export default {
 
 
 <style lang="scss" scoped>
-
 .box {
   max-width: 800px;
 }
